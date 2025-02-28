c88d0473a95ddc0beecd18cccbeab379bfa1bd13
Darek <dariusz.kralewski@ug.edu.pl>
2025-02-28 13:05:00 +0100

    koniec zajęć

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 / 5);    // output: 2
+            Console.WriteLine(-13 / 5);   // output: -2
+            Console.WriteLine(13 / -5);   // output: -2
+            Console.WriteLine(-13 / -5);  // output: 2
+
+            Console.WriteLine(13 / 5.0);       // output: 2.6
+
+            int a = 13;
+            int b = 5;
+            Console.WriteLine((double)a / b);  // output: 2.6
+
+            Console.WriteLine(5 % 4);   // output: 1
+            Console.WriteLine(5 % -4);  // output: 1
+            Console.WriteLine(-5 % 4);  // output: -1
+            Console.WriteLine(-5 % -4); // output: -1
+        }
+    }
+}
---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący boki prostokąta i wypisujący na ekranie jego pole i obwód.
+
+
+        }
+    }
+}
